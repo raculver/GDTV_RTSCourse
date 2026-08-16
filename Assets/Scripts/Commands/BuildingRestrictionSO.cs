1b6d5d8327f86019bd17dd3b883c79873e8dd2ea
robert.culver <robertculver90@gmail.com>
2026-08-16 12:18:20 +0100

    extending building restrictions. No build on supplies or other buildings

--- Assets/Scripts/Commands/BuildingRestrictionSO.cs
+++ Assets/Scripts/Commands/BuildingRestrictionSO.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -6,22 +7,49 @@ namespace GameDevTV.RTS.Commands
     [CreateAssetMenu(fileName = "Building Restriction", menuName = "Buildings/Restrictions", order = 7)]
     public class BuildingRestrictionSO: ScriptableObject
     {
+        [field: SerializeField] public Vector3 HitExtents {get; private set;} = Vector3.one;
+
+        [Header("Placement Hit Detection Checks")]
+        [field: SerializeField] public float HitRadius {get; private set;} = 1f;
+        [field: SerializeField] public LayerMask LayersToCheck {get; private set;}
+        [field: SerializeField] public OverlapStyle HitDetectionStyle = OverlapStyle.Sphere;
+        
+        [Header("NavMesh Checks")]
         [field: SerializeField] public bool MustBeFullyOnNavmesh{get; private set;} = true;
         [field: SerializeField] public int NavMeshAgentTypeId {get; private set;}      
         [field: SerializeField] public float NavMeshTolerance {get; private set;} = 0.1f;
 
-        [field: SerializeField] public Vector3 Extents {get; private set;} = Vector3.one;
+        private Collider[] hitColliders = new Collider[1]; // in our useage here we only care if a single object was hit.
 
         public bool CanPlace(Vector3 position){
-            return (!MustBeFullyOnNavmesh) || FourCornersOnNavMesh(position);
+            bool navMeshChecks = (!MustBeFullyOnNavmesh) || FourCornersOnNavMesh(position);
+            bool placementHitChecks = GetPlacementHitChecks(position);
+
+            return navMeshChecks && placementHitChecks;
+        }
+
+        private bool GetPlacementHitChecks(Vector3 position)
+        {
+            return HitDetectionStyle switch
+            {
+                OverlapStyle.Sphere => Physics.OverlapSphereNonAlloc(position, HitRadius, hitColliders, LayersToCheck) == 0,
+                OverlapStyle.Box => Physics.OverlapBoxNonAlloc(position, HitExtents, hitColliders, Quaternion.identity, LayersToCheck) == 0,
+                _ => throw new System.NotImplementedException()
+            };
         }
 
         private bool FourCornersOnNavMesh(Vector3 position){
             NavMeshQueryFilter query = new(){areaMask = NavMesh.AllAreas, agentTypeID = NavMeshAgentTypeId};
-            return NavMesh.SamplePosition(position + new Vector3( Extents.x, 0,  Extents.z), out NavMeshHit _, NavMeshTolerance, query)
-                && NavMesh.SamplePosition(position + new Vector3( Extents.x, 0, -Extents.z), out NavMeshHit _, NavMeshTolerance, query)
-                && NavMesh.SamplePosition(position + new Vector3(-Extents.x, 0,  Extents.z), out NavMeshHit _, NavMeshTolerance, query)
-                && NavMesh.SamplePosition(position + new Vector3(-Extents.x, 0, -Extents.z), out NavMeshHit _, NavMeshTolerance, query);
+            return NavMesh.SamplePosition(position + new Vector3( HitExtents.x, 0,  HitExtents.z), out NavMeshHit _, NavMeshTolerance, query)
+                && NavMesh.SamplePosition(position + new Vector3( HitExtents.x, 0, -HitExtents.z), out NavMeshHit _, NavMeshTolerance, query)
+                && NavMesh.SamplePosition(position + new Vector3(-HitExtents.x, 0,  HitExtents.z), out NavMeshHit _, NavMeshTolerance, query)
+                && NavMesh.SamplePosition(position + new Vector3(-HitExtents.x, 0, -HitExtents.z), out NavMeshHit _, NavMeshTolerance, query);
+        }
+
+
+        public enum OverlapStyle{
+            Sphere,
+            Box
         }
     }
 }
