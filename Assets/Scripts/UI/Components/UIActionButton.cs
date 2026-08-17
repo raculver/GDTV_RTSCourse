f97a6fec133e04e1dfdfd3dccf303a798e4622ad
robert.culver <robertculver90@gmail.com>
2026-08-17 22:07:39 +0100

    first pass tooltips

--- Assets/Scripts/UI/Components/UIActionButton.cs
+++ Assets/Scripts/UI/Components/UIActionButton.cs
@@ -1,15 +1,20 @@
+using System;
 using GameDevTV.RTS.Commands;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace GameDevTV.RTS.UI.Components{
 
 [RequireComponent(typeof (Button))]
-public class UIActionButton : MonoBehaviour, IUIElement<BaseCommand, UnityAction>
+public class UIActionButton : MonoBehaviour, IUIElement<BaseCommand, UnityAction>, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private Image icon;
+    [SerializeField] private GameObject tooltipText;
+    
     private Button button;
+    private float tooltipWaitTime { get;} = 0.8f;
 
     void Awake(){
         button = GetComponent<Button>();
@@ -39,5 +44,20 @@ public class UIActionButton : MonoBehaviour, IUIElement<BaseCommand, UnityAction
             this.icon.enabled = true;
         }
     }
+
+    public void OnPointerEnter(PointerEventData eventData){
+        // invoke calls after a certain delay
+        Invoke(nameof(ShowTooltip), tooltipWaitTime);
+    }
+
+    public void OnPointerExit(PointerEventData eventData){
+        // cancel pending invocation
+        CancelInvoke(nameof(ShowTooltip));
+        HideToolTip();
+    }
+
+    private void ShowTooltip() => tooltipText.SetActive(true);
+    private void HideToolTip() => tooltipText.SetActive(false);
+
 }
 }
