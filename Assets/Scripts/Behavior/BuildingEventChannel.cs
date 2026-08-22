b5805789cf1df93a144a6fdaa72430a02c9f2735
robert.culver <robertculver90@gmail.com>
2026-08-22 11:10:38 +0100

    event type successfully added to worker BT without absolute full spectrum breakage.

--- Assets/Scripts/Behavior/BuildingEventChannel.cs
+++ Assets/Scripts/Behavior/BuildingEventChannel.cs
@@ -4,6 +4,8 @@ using Unity.Behavior;
 using UnityEngine;
 using Unity.Properties;
 
+namespace GameDevTV.RTS.Behahavior{
+
 #if UNITY_EDITOR
 [CreateAssetMenu(menuName = "Behavior/Event Channels/Building Event Channel")]
 #endif
@@ -11,3 +13,4 @@ using Unity.Properties;
 [EventChannelDescription(name: "Building Event Channel", message: "[Self] [BuildingEventType] on [BaseBuilding] .", category: "Events", id: "2e78e38a6c5afbba83032f5a52da8445")]
 public sealed partial class BuildingEventChannel : EventChannel<GameObject, BuildingEventType, BaseBuilding> { }
 
+}
